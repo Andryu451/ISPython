723ad57f76575dfb3e29663ec4686d54f4e222c9
Andryu451 <andryu.beshanov@gmail.com>
2025-01-18 21:31:35 +0300

    Циклы ч.2

--- Skillfactory/Циклы и условия/Циклы.py
+++ Skillfactory/Циклы и условия/Циклы.py
@@ -45,8 +45,8 @@ while True:
 
 # плохо
 n = 1
-while n < 10:  # в данной программе это условие всегда True, цикл будет бесконечным
-    print("Hello World")
+# while n < 10:  # в данной программе это условие всегда True, цикл будет бесконечным
+    # print("Hello World")
 
 # хорошо
 n = 1
@@ -55,5 +55,61 @@ while True:  # в данной программе это условие всег
    n += 1
    if n > 10:  # условие, при достижении которого цикл while будет принудительно завершён
        break
+
+N = 2
+M = 3
+# заполнили матрицу последовательными числами
+matrix = [
+    [0, 1, 2], 
+    [3, 4, 5], 
+]
+
+
+for i in range(N):  # цикл, отвечающий за строки
+    for j in range(M):  # цикл, отвечающий за столбцы
+        print(matrix[i][j], end=" ")
+# 0 1 2 3 4 5
+
+for i in range(N): 
+    for j in range(M): 
+        print(matrix[i][j], end=" ")
+    print()  # перенос на новую строку
+
+# 0 1 2
+# 3 4 5
+
+random_matrix = [
+   [9, 2, 1],
+   [2, 5, 3],
+   [4, 8, 5]
+]
+
+mean_value_rows = []  # здесь будут храниться средние значения для каждой строки
+min_value_rows = []  # здесь будут храниться минимальные значения для каждой строки
+min_index_rows = []  # здесь будут храниться индексы минимальных значений для каждой строки
+max_value_rows = []  # здесь будут храниться максимальные значения для каждой строки
+max_index_rows = []  # здесь будут храниться индексы максимальных значений для каждой строки
+
+for row in random_matrix:  # здесь мы целиком берём каждую сроку
+   min_index = 0  # в качестве минимального значения возьмём первый элемент строки
+   max_index = 0
+   min_value = row[min_index]  # начальное минимальное значение для каждой строки будет новое
+   max_value = row[max_index]  # для максимального значения то же самое
+   for index_col in range(len(row)):
+       if row[index_col] < min_value:
+           min_value = row[index_col]
+           min_index = index_col
+       if row[index_col] > max_value:
+           max_value = row[index_col]
+           max_index = index_col
+   min_value_rows.append(min_value)
+   min_index_rows.append(min_index)
+   max_value_rows.append(max_value)
+   max_index_rows.append(max_index)
+  
+print(min_value_rows)
+print(min_index_rows)
+print(max_value_rows)
+print(max_index_rows)
 
 
